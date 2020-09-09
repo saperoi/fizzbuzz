8db170771cdf75a2d5d17a8d8e15191071e97d48
saperoi <bizgames.shawn@gmail.com>
2020-09-09 19:25:42 +0200

    Create javascript to website.js

---
+++ javascript to website.js
@@ -0,0 +1,15 @@
+var i = 1;
+
+while (i<=100) {
+	if (i%3===0 && i%5===0) {
+		document.write("FizzBuzz");
+	} else if (i%3===0) {
+		document.write("Fizz");
+	} else if (i%5===0) {
+		document.write("Buzz");
+	} else {
+		document.write(i);
+	}
+	i++;
+	document.write("<br>");
+}
