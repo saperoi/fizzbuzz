8fa59e7bec73958574eee740c542253088641300
saperoi <bizgames.shawn@gmail.com>
2020-09-09 14:39:50 +0200

    Update arduino lcd.ino

--- arduino lcd.ino
+++ arduino lcd.ino
@@ -1,6 +1,6 @@
 #include <LiquidCrystal.h>
 
-unsigned int t;
+int t = 1;
 String spaceempty = "       ";
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
@@ -10,15 +10,19 @@ void setup() {
 }
 
 void loop() {
-  t=1+millis()/200;
   lcd.setCursor(0,0);
-  if (t%3==0 && t%5==0) {
-    lcd.print("FizzBuzz");
-  } else if (t%3==0) {
-    lcd.print("Fizz    ");
-  } else if (t%5==0) {
-    lcd.print("Buzz    ");
-  } else {
-    lcd.print(t + spaceempty);
+  while (t<=100) { // Replace 100 here if you want it to go up to a different number
+    if (t%3==0 && t%5==0) {
+      lcd.print("FizzBuzz");
+    } else if (t%3==0) {
+      lcd.print("Fizz    ");
+    } else if (t%5==0) {
+      lcd.print("Buzz    ");
+    } else {
+      lcd.print(t + spaceempty);
+    }
+    t++;
+    delay(1000);
+    lcd.setCursor(0,0);
   }
 }
