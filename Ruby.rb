a1849fe4c7d0d81c5b57fd4f1e86b98bc6c151fb
saperoi <bizgames.shawn@gmail.com>
2020-09-12 15:46:31 +0200

    Update Ruby.rb

--- Ruby.rb
+++ Ruby.rb
@@ -4,16 +4,12 @@ while i <= 100
   result = ""
   if i % 3 == 0
     result += result + "Fizz"
-  else
-    result = result
   end
   if i % 5 == 0
     result += "Buzz"
-  else
-    result = result
   end
   if result == ""
-   puts i
+    puts i
   else
     puts result
   end
