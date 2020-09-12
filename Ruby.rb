12e9e313e8f1008ce63e9a53ca1a21c959711bf6
saperoi <bizgames.shawn@gmail.com>
2020-09-12 15:45:34 +0200

    Update Ruby.rb

--- Ruby.rb
+++ Ruby.rb
@@ -1,13 +1,21 @@
 i = 1
+
 while i <= 100
-  if i % 3 == 0 && i % 5 == 0
-    puts "FizzBuzz"
-  elsif i % 3 == 0
-   puts "Fizz"
-  elsif i % 5 == 0
-    puts "Buzz"
+  result = ""
+  if i % 3 == 0
+    result += result + "Fizz"
   else
+    result = result
+  end
+  if i % 5 == 0
+    result += "Buzz"
+  else
+    result = result
+  end
+  if result == ""
    puts i
+  else
+    puts result
   end
   i += 1
   sleep 1
